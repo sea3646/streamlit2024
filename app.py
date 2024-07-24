78ad6fc5696d6ceb5d0a26e07d51ec7f7594b7f7
= <=>
2024-07-24 11:51:38 +0900

    app.이름

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import random
 
 st.header('주사위 게임', divider='rainbow')
 st.button('주사위던지기', type='primary')
+if clicked :
+    n= random.randint(1,6)
+    st.image(f' ./img/{n}.png')
